0638c57009595db63bdc8846ceb5f0d793500404
Melkov Denis <d.a.melkov@gmail.com>
2020-07-21 13:33:50 +0300

    fix activity name

--- app/src/main/java/ru/startandroid/develop/melkovhw52/NotesActivity.java
+++ app/src/main/java/ru/startandroid/develop/melkovhw52/NotesActivity.java
@@ -20,7 +20,7 @@ public class NotesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notes);
 
         initViews();
         getDateFromSharedPref();
